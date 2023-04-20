0b221d97a649b858347fa96b96666fa52fff2394
Andy Wardley <abw@wardley.org>
2023-04-20 17:29:25 +0100

    Context submit

--- src/pages/form/Submit/examples/Submit.jsx
+++ src/pages/form/Submit/examples/Submit.jsx
@@ -5,8 +5,8 @@ import React from 'react'
 // PRETEND: import { Form, Field, Submit } from '@abw/react-formula'
 
 const SubmitExample = () =>
-  <Form showRequired onSubmit={ submit => console.log('Form submitted: ', submit) }>
-    <Field name="one" label="Field one" required/>
+  <Form showRequired onSubmit={ submit => console.log('Form submitted: ', submit) } debug>
+    <Field name="one" label="Field one" required debug/>
     <Field name="two" label="Field two" debug/>
     <Submit/>
     <Debug/>
