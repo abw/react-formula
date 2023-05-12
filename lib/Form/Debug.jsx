d21fd1b30e7ee6a267e373c0ae91f8e486dd5b40
Andy Wardley <abw@wardley.org>
2023-05-12 15:17:24 +0100

    Debug panel

--- lib/Form/Debug.jsx
+++ lib/Form/Debug.jsx
@@ -3,20 +3,20 @@ import { useForm } from './Context.js'
 import { capitalise } from '@abw/badger-utils'
 import { Themed } from '../Theme.jsx'
 
-const showable = ['values', 'config', 'status', 'fields']
+const showable = ['values', 'status', 'fields']
 
-export const Debug = ({show={ values: true }, showAll=false}) => {
+export const Debug = ({show={ values: true }, showAll=false, ...props}) => {
   const form = useForm()
   return (
     <div className="debug">
       { showable.map(
-        item => (showAll || show[item]) &&
-          <React.Fragment key={item}>
+        item => (props[item] ?? (showAll || show[item])) &&
+          <div className="panel" key={item}>
             <h4>{capitalise(item)}</h4>
             <pre>
               {JSON.stringify(form[item], null, 2)}
             </pre>
-          </React.Fragment>
+          </div>
       )}
     </div>
   )
