2eca8cb5480dc64684b8d00d7b327dae13a5afad
Andy Wardley <abw@wardley.org>
2023-05-21 08:17:56 +0100

    Utility CSS classes for alignment

--- website/test/Layout.jsx
+++ website/test/Layout.jsx
@@ -4,8 +4,12 @@ import { Form, Field, Submit } from '../../lib/index.js'
 const LayoutTest = () =>
   <div>
     <h1>Layout Test</h1>
+    <p>
+      This is a test to ensure that all fields are the same size and line
+      up properly with each other.  This is no mean feat.
+    </p>
     <Form>
-      <div className="grid-2 end gap-h-2">
+      <div className="grid-2 start gap-h-2">
         <Field
           name="one"
           label="Text Field"
@@ -51,11 +55,33 @@ const LayoutTest = () =>
           label="Number Field"
           type="number"
         />
+      </div>
+      <p>
+        These components are aligned to the start of the grid container.
+        The submit button has the <code>mar-t-label</code> class which adds
+        a margin to the top equivalent to the height of the label and margin
+        between the label and input.  The input and button should align
+        perfectly.
+      </p>
+      <div className="grid-2 start gap-h-2">
         <Field
           name="eleven"
-          label="Text Field - it's one louder"
+          label="Button has mar-t-label"
         />
-        <Submit/>
+        <Submit className="mar-t-label"/>
+      </div>
+      <p>
+        These components are aligned to the end of the grid container.
+        The submit button has the <code>mar-b-field</code> class which adds
+        a margin to the bottom equivalent to the margin on the bottom of the
+        input field.  They should also align perfectly.
+      </p>
+      <div className="grid-2 end gap-h-2">
+        <Field
+          name="eleven"
+          label="Button has mar-b-field"
+        />
+        <Submit className="mar-b field"/>
       </div>
     </Form>
   </div>
